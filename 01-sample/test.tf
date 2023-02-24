c3d7d015a41237582cdc0a3d6fdb0a3f507c8a13
Srini K <srinikdevops0131@gmail.com>
2023-02-24 07:43:01 -0800

    updating ./learn-terraform

---
+++ 01-sample/test.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "ec2" {
+  ami = "ami-0a017d8ceb274537d"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-097c3942d3049b419"]
+  tags = {
+    Name = "test"
+  }
+}
