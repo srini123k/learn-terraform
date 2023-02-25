d970df0205c18fbb7f029a3a9822f381f1d0cd79
Srini K <srinikdevops0131@gmail.com>
2023-02-24 16:16:49 -0800

    updating ./learn-terraform

--- 04-variable/var.tf
+++ 04-variable/var.tf
@@ -17,3 +17,35 @@ variable "cli" {}
 output "cli" {
   value = var.cli
 }
+
+
+variable "sample5" {
+  default = "Hello"
+}
+
+
+variable "sample6" {
+  default = "Hello"
+}
+
+
+variable "sample7" {
+  default = [
+    "Hello",
+    1000,
+    true,
+    "world"]
+}
+
+
+variable "sample8" {
+  default = {
+   string = "Hello",
+    number = 1000,
+    boolean = true,
+  }
+}
+
+output "types" {
+  value = "Variable sample6 - ${var.sample6}, first value in list - ${var.sample7[0]}, Boolean value of - ${var.sample8[boolean]}  "
+}
