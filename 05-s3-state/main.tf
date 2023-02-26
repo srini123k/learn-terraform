1806ae68b049a22c629bbed9f6be641b45c08de1
Srini K <srinikdevops0131@gmail.com>
2023-02-25 22:02:18 -0800

    updating ./learn-terraform

---
+++ 05-s3-state/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-b71.cloud"
+    key    = "05-s3-state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-0a017d8ceb274537d"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-097c3942d3049b419"]
+  tags = {
+    Name = "test"
+  }
+}
