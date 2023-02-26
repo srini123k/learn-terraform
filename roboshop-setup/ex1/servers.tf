57ddba62c8465de9cfd1889c928da1a3213b8ba8
Srini K <srinikdevops0131@gmail.com>
2023-02-25 22:16:04 -0800

    updating ./learn-terraform

--- roboshop-setup/ex1/servers.tf
+++ roboshop-setup/ex1/servers.tf
@@ -1,3 +1,9 @@
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
 resource "aws_instance" "frontend" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
---
+++ roboshop-setup/ex2/main.tf
@@ -0,0 +1,13 @@
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_instance" "frontend" {
+  count = 5
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-097c3942d3049b419"]
+  }
+}
