4a2ed6e3585ca750e7c22a8bac74e5de87abff91
Srini K <srinikdevops0131@gmail.com>
2023-02-25 22:35:34 -0800

    updating ./learn-terraform

--- roboshop-setup/ex2/main.tf
+++ roboshop-setup/ex2/main.tf
@@ -9,6 +9,9 @@ resource "aws_instance" "frontend" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-097c3942d3049b419"]
+  tags = {
+    Name = var.instances[count.index]
+  }
   }
 
 variable "instances" {
