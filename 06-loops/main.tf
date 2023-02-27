eae4b31f91e6d516fcbbb5e5b3267d2f37c83de8
Srini K <srinikdevops0131@gmail.com>
2023-02-26 17:00:04 -0800

    updating ./learn-terraform

--- 06-loops/main.tf
+++ 06-loops/main.tf
@@ -5,3 +5,11 @@ variable "d1" {
 resource "null_resource" "nothing" {
   count = var.d1
 }
+
+variable "d2" {
+  default = [0, 1, 3, 2, 4]
+}
+
+resource "null_resource" "d2" {
+  count = length(var.d2)
+}
