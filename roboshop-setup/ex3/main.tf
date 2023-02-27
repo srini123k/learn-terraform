f93ac5f3b38f6f40eae22df79a3247e8452a187b
Srini K <srinikdevops0131@gmail.com>
2023-02-26 17:12:34 -0800

    updating ./learn-terraform

---
+++ roboshop-setup/ex3/main.tf
@@ -0,0 +1,29 @@
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_instance" "frontend" {
+  count                  = length(var.instances)
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-097c3942d3049b419"]
+  tags = {
+    Name = var.instances[count.index]
+  }
+}
+
+variable "instances" {
+  default = {
+    catalogue={
+      name="catalogue"
+      type="t3.micro"
+    }
+    user={
+      name="user"
+      type="t3.small"
+    }
+  }
+}
+
