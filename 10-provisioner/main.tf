bc1cca3c4a3a5739529fa00889097e8013c3b310
Srini K <srinikdevops0131@gmail.com>
2023-03-01 09:09:33 -0800

    updating ./learn-terraform

---
+++ 10-provisioner/main.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "ec2" {
+  ami = "ami-0a017d8ceb274537d"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-097c3942d3049b419"]
+  tags = {
+    Name = "test"
+  }
+}
+
+provisioner "remote-exec"{
+
+connection {
+host =self.public_ip
+user="root"
+password="DevOps321"
+}
+inline =[
+"echo Hello"
+]
+}
