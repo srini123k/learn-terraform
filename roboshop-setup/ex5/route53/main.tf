42ee5427e37d38874929d232c1a5fcf30224ee7f
Srini K <srinikdevops0131@gmail.com>
2023-02-28 22:13:05 -0800

    updating ./learn-terraform

--- roboshop-setup/ex5/route53/main.tf
+++ roboshop-setup/ex5/route53/main.tf
@@ -3,7 +3,7 @@ resource "aws_route53_record" "record" {
   name    = "${var.component}-dev.devopsb71.cloud"
   type    = "A"
   ttl     = 300
-  records = [aws_instance.ec2.private_ip]
+  records = [var.private_ip]
 }
 
 variable "private_ip" {}
