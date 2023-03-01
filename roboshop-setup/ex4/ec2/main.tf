8866c2fa27217ef653de626f820729cf4943ce79
Srini K <srinikdevops0131@gmail.com>
2023-02-28 21:26:12 -0800

    updating ./learn-terraform

--- roboshop-setup/ex4/ec2/main.tf
+++ roboshop-setup/ex4/ec2/main.tf
@@ -7,7 +7,7 @@ data "aws_ami" "ami" {
 resource "aws_instance" "ec2" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = var.instance_type
-  vpc_security_group_ids = ["sg-097c3942d3049b419"]
+  vpc_security_group_ids = [var.sg_id]
   tags = {
     Name = var.component
   }
@@ -24,3 +24,5 @@ resource "aws_route53_record" "record" {
 variable "component" {}
 
 variable "instance_type" {}
+
+variable "sg_id" {}
--- roboshop-setup/ex4/main.tf
+++ roboshop-setup/ex4/main.tf
@@ -1,8 +1,9 @@
 module "ec2" {
-  for_each = var.instances
-  source = "./ec2"
-  component= each.value["name"]
-  instance_type=each.value["type"]
+  for_each      = var.instances
+  source        = "./ec2"
+  component     = each.value["name"]
+  instance_type = each.value["type"]
+  sg_id            = module.sg.sg_id
 }
 
 module "sg" {
@@ -12,12 +13,12 @@ module "sg" {
 variable "instances" {
   default = {
     catalogue={
-      name="catalogue"
-      type="t3.micro"
+      name       = "catalogue1"
+      type       = "t3.micro"
     }
     user={
-      name="user"
-      type="t3.small"
+      name       = "user1"
+      type       = "t3.small"
     }
   }
 }
