79fd783b1954794e649c3d12298af2553d6c0a60
Srini K <srinikdevops0131@gmail.com>
2023-03-09 09:57:04 -0800

    updating ./learn-terraform

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -8,6 +8,12 @@ resource "aws_spot_instance_request" "node1" {
   }
 }
 
+resource "aws_ec2_tag" "node1" {
+  resource_id = aws_spot_instance_request.node1.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-server"
+}
+
 resource "aws_spot_instance_request" "node2" {
   ami           = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
@@ -16,4 +22,10 @@ resource "aws_spot_instance_request" "node2" {
   tags = {
     Name = "prom-test-node"
   }
+}
+
+resource "aws_ec2_tag" "node2" {
+  resource_id = aws_spot_instance_request.node2.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-node"
 }
